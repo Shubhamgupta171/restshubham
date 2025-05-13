7205dbcc901c89849127e20328398af77c8c457d
Shubham Gupta <78054098+Shubhamgupta171@users.noreply.github.com>
2025-05-13 20:24:16 +0530

    Add files via upload

---
+++ app.config.ts
@@ -0,0 +1,7 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { UsersComponent } from '/Users/shubhamgupta/Desktop/Deloite/angular-crud-frontend/src/app/users/users.component';
+
+export const appConfig: ApplicationConfig = {
+  providers: []
+};
